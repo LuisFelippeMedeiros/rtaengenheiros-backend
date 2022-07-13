e0a820873426de4d62e721d595c3014ae3dbcc9f
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2022-07-13 16:25:15 -0400

    feat(auth): Implementando autenticação JWT e criptografia da senha
    Implementando JWT e criptografia da senha

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { UnauthorizedError } from './errors/unauthorized.error';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      name: user.name,
+      email: user.email,
+      cpf: user.cpf,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new UnauthorizedError(
+      'Usuário e/ou senha não correspondem ao cadastrado em nossa base de dados.',
+    );
+  }
+}
