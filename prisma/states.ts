c2949f3c567d1d0f1165af07b1d1141540943241
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2022-07-19 14:16:43 -0400

    Adding city/state table
    Adding city and state table, also seeds for those tables

---
+++ prisma/states.ts
@@ -0,0 +1,55 @@
+export const states = [
+  { id: 1, initials: 'RO', name: 'Rondônia', active: true },
+
+  { id: 2, initials: 'AC', name: 'Acre', active: true },
+
+  { id: 3, initials: 'AM', name: 'Amazonas', active: true },
+
+  { id: 4, initials: 'RR', name: 'Roraima', active: true },
+
+  { id: 5, initials: 'PA', name: 'Pará', active: true },
+
+  { id: 6, initials: 'AP', name: 'Amapá', active: true },
+
+  { id: 7, initials: 'TO', name: 'Tocantins', active: true },
+
+  { id: 8, initials: 'MA', name: 'Maranhão', active: true },
+
+  { id: 9, initials: 'PI', name: 'Piauí', active: true },
+
+  { id: 10, initials: 'CE', name: 'Ceará', active: true },
+
+  { id: 11, initials: 'RN', name: 'Rio Grande do Norte', active: true },
+
+  { id: 12, initials: 'PB', name: 'Paraíba', active: true },
+
+  { id: 13, initials: 'PE', name: 'Pernambuco', active: true },
+
+  { id: 14, initials: 'AL', name: 'Alagoas', active: true },
+
+  { id: 15, initials: 'SE', name: 'Sergipe', active: true },
+
+  { id: 16, initials: 'BA', name: 'Bahia', active: true },
+
+  { id: 17, initials: 'MG', name: 'Minas Gerais', active: true },
+
+  { id: 18, initials: 'ES', name: 'Espírito Santo', active: true },
+
+  { id: 19, initials: 'RJ', name: 'Rio de Janeiro', active: true },
+
+  { id: 20, initials: 'SP', name: 'São Paulo', active: true },
+
+  { id: 21, initials: 'PR', name: 'Paraná', active: true },
+
+  { id: 22, initials: 'SC', name: 'Santa Catarina', active: true },
+
+  { id: 23, initials: 'RS', name: 'Rio Grande do Sul', active: true },
+
+  { id: 24, initials: 'MS', name: 'Mato Grosso do Sul', active: true },
+
+  { id: 25, initials: 'MT', name: 'Mato Grosso', active: true },
+
+  { id: 26, initials: 'GO', name: 'Goiás', active: true },
+
+  { id: 27, initials: 'DF', name: 'Distrito Federal', active: true },
+];
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -25,18 +17,18 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.userService.findOne(+id);
-  // }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @Get(':name')
+  findOne(@Param('name') name: string) {
+    return this.userService.findByName(name);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Put('deactivate/:id')
+  deactivate(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
   }
 }
