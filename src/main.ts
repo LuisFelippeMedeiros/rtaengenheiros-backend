c73c89ad9e068239aac0965c1df673d91a27ed12
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2022-10-26 16:35:40 -0400

    Fnal

--- src/main.ts
+++ src/main.ts
@@ -34,12 +34,6 @@ async function bootstrap() {
   });
 
   app.enableCors();
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
