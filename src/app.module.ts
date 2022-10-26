a2e5a2fe97c85272b36e0ffa102706114055af17
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2022-10-26 09:48:31 -0400

    Teste front
    Teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,16 +39,16 @@ import { MulterExtendedModule } from 'nestjs-multer-extended';
     BillToPayModule,
     PurchaseRequestProductModule,
     ConfigModule.forRoot(),
-    MulterExtendedModule.register({
-      awsConfig: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-      },
-      bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
-      basePath: 's3://rtaengenheiros-backend/avatar/',
-      // fileSize: 1 * 1024 * 1024,
-    }),
+    // MulterExtendedModule.register({
+    //   awsConfig: {
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //     region: process.env.AWS_REGION,
+    //   },
+    //   bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
+    //   basePath: 's3://rtaengenheiros-backend/avatar/',
+    //   // fileSize: 1 * 1024 * 1024,
+    // }),
   ],
   controllers: [],
   providers: [
