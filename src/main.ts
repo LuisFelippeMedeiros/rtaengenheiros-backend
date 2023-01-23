cd9030ed1d2200581bcc3c5ae3c6ad9e9e04175b
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2023-01-23 15:55:43 -0400

    Habilitando Chamada AWS

--- src/main.ts
+++ src/main.ts
@@ -33,11 +33,11 @@ async function bootstrap() {
   );
 
   //Configuração Bucket AWS
-  // config.update({
-  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  //   region: process.env.AWS_REGION,
-  // });
+  config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
 
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
