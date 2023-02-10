90776fd43cc7da6a3ba5b7c441a2341a413b3df8
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2023-02-10 10:38:21 -0400

    Subindo alterações de supplier

---
+++ prisma/seeds/suppliers.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const suppliers = [
+  {
+    id: uuidv4(),
+    name: 'teste',
+    cnpj: '00.000.000/0001-00',
+    ie: '000.000.000',
+    telephone: '65 9 8110-6000',
+    account: '0000',
+    address: 'Rua Teste',
+    bank: 'Do Brasil',
+    complement: 'Não possui',
+    district: 'Jardim América',
+    number: '15',
+    pix: '00.000.000/0001-00',
+    pix2: '00.000.000/0001-00',
+    active: true,
+    agency: '0016',
+    email: 'testefornecedor@gmail.com',
+    company_id: '87900226-8065-4c87-afcc-bf4b1a59d9e6',
+  },
+];
