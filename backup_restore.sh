f4036c61357d1aa10f568872166b386ecdc1e69a
luisfelippemedeiros <lfelippemedeiros@gmail.com>
2023-10-23 16:48:24 -0400

    Script

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Não precisa carregar .env no Heroku, as variáveis de ambiente já estão configuradas
-# export $(egrep -v '^#' .env | xargs)
-
-# Passo 1: Backup (ajuste conforme necessário)
-BACKUP_URL=$(heroku pg:backups:capture --app $PROD_APP_NAME --json | jq -r '.url')
-
-# Verifique se a URL do backup foi obtida com sucesso
-if [ -z "$BACKUP_URL" ]; then
-  echo "Erro: Não foi possível obter a URL do backup."
-  exit 1
-fi
+# Substitua pelo URL do seu backup
+BACKUP_URL=${pg:backups:url --app $PROD_APP_NAME}
 
 # Passo 2: Restauração em Develop (ajuste conforme necessário)
-heroku pg:backups:restore $BACKUP_URL DATABASE_URL --app $DEV_APP_NAME --confirm $DEV_APP_NAME
+pg_restore --verbose --clean --no-acl --no-owner -h $DEV_SERVER -U $DEV_USERNAME -d $DEV_DATABASE "$BACKUP_URL"
 
 # Passo 3: Restauração Local (ajuste conforme necessário)
 curl -o latest.dump "$BACKUP_URL"
-docker cp latest.dump $CONTAINER_ID_OR_NAME:/latest.dump
-docker exec $CONTAINER_ID_OR_NAME pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $DB_USER -d $DB_NAME /latest.dump
+# docker cp latest.dump $CONTAINER_ID_OR_NAME:/latest.dump
+# docker exec $CONTAINER_ID_OR_NAME pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $DB_USER -d $DB_NAME /latest.dump
 
 # Cleanup
 rm latest.dump
